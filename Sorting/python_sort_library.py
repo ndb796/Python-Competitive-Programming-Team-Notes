f8a17c678f259db79a26ef1f05ef20c01d20ec73
Dongbin Na <ndb796@naver.com>
2020-04-08 23:02:30 +0900

    Update python_sort_library.py

--- Sorting/python_sort_library.py
+++ Sorting/python_sort_library.py
@@ -1,8 +1,29 @@
-n = 5
-data = [8, 5, 4, 7, 2]
 
-''' Python Sort Library '''
+n = 5
+
+''' Python Sort Library (Basic) '''
+data = [8, 5, 4, 7, 2]
 data.sort()
 
 for x in data:
     print(x)
+
+print("---------------")
+
+''' Python Sort Library (Based on a key) '''
+data = [(25, 'Na'), (20, 'Kim'), (23, 'Seo'), (28, 'Park'), (20, 'Ahn')]
+data.sort(key=lambda x: x[0]) # Stable Sort (When using a key)
+
+for x in data:
+    print(x)
+
+print("---------------")
+
+''' Python Sort Library '''
+data = [(25, 'Na'), (20, 'Kim'), (23, 'Seo'), (28, 'Park'), (20, 'Ahn')]
+data.sort() # Non-stable Sort (When not using a key)
+
+for x in data:
+    print(x)
+
+print("---------------")
