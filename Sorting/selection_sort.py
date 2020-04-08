a33b97c91b526603952e9a9d1faa30dd89930266
Dongbin Na <ndb796@naver.com>
2020-04-08 22:07:21 +0900

    Update selection_sort.py

--- Sorting/selection_sort.py
+++ Sorting/selection_sort.py
@@ -3,11 +3,11 @@ data = [8, 5, 4, 7, 2]
 
 ''' Selection Sort '''
 for i in range(n):
-    min_index = i # the index of the minimum value
+    min_index = i # The index of the minimum value.
     for j in range(i + 1, n):
         if data[min_index] > data[j]:
             min_index = j
-    data[i], data[min_index] = data[min_index], data[i] # swap
+    data[i], data[min_index] = data[min_index], data[i] # Swap.
 
 for i in data:
     print(i)
