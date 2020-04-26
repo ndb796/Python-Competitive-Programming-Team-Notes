69cb04c8a53be1cd9191320fffe64f95d4405248
Dongbin Na <ndb796@naver.com>
2020-04-27 03:57:44 +0900

    Update number_of_intersection_points_of_two_lines_in_1_dimension.py

--- Geometry/number_of_intersection_points_of_two_lines_in_1_dimension.py
+++ Geometry/number_of_intersection_points_of_two_lines_in_1_dimension.py
@@ -1,4 +1,4 @@
-# Number of intersection points of two lines in 1 dimension
+''' Number of intersection points of two lines in 1 dimension '''
 def num_of_intersect_two_lines(line_1, line_2):
     return max(min(line_1[1], line_2[1]) - max(line_1[0], line_2[0]) + 1, 0)
 
