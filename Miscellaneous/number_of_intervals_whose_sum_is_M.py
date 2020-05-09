f09941729e718b1bfbef2518404ff55de5db28a7
Dongbin Na <ndb796@naver.com>
2020-05-10 07:20:46 +0900

    Create number_of_intervals_whose_sum_is_M.py

---
+++ Miscellaneous/number_of_intervals_whose_sum_is_M.py
@@ -0,0 +1,21 @@
+''' Number of intervals whose sum is M. '''
+n = 5
+m = 5
+data = [1, 2, 3, 2, 5]
+
+count = 0
+interval_sum = 0
+end = 0
+
+# move 'start' pointer to the right.
+for start in range(n):
+    # move 'end' pointer to the right.
+    while interval_sum < m and end < n:
+        interval_sum += data[end]
+        end += 1
+    # If the sum of interval is 'm', count the number.
+    if interval_sum == m:
+        count += 1
+    interval_sum -= data[start]
+
+print(count)
