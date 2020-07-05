de9d37ecbc5f715c53a639012f254ee006ba994d
Dongbin Na <ndb796@naver.com>
2020-07-05 10:09:06 +0900

    Update binary_search.py

--- Searching/binary_search.py
+++ Searching/binary_search.py
@@ -1,20 +1,23 @@
 n = 10
 target = 13
-data = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+array = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
 
 ''' Binary Search (Iterative Method) '''
-def binary_search(start, end, target):
+def binary_search(array, target, start, end):
     while start <= end:
         mid = (start + end) // 2
-        if data[mid] == target:
+        # If the target is found, return the mid index.
+        if array[mid] == target:
             return mid
-        elif data[mid] < target:
-            start = mid + 1
-        else:
+        # If the value of the mid index is greater than the target, search the left part.
+        elif array[mid] > target:
             end = mid - 1
+        # If the value of the mid index is smaller than the target, search the right part.
+        else:
+            start = mid + 1
     return None
 
-result = binary_search(0, n - 1, target)
+result = binary_search(array, target, 0, n - 1)
 if result == None:
     print(None)
 else:
