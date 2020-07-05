81e498f24b6709d044122b0052e2647bea217a8a
Dongbin Na <ndb796@naver.com>
2020-07-05 10:24:44 +0900

    Update binary_search.py

--- Searching/binary_search.py
+++ Searching/binary_search.py
@@ -1,7 +1,3 @@
-n = 10
-target = 13
-array = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
-
 ''' Binary Search (Iterative Method) '''
 def binary_search(array, target, start, end):
     while start <= end:
@@ -16,6 +12,10 @@ def binary_search(array, target, start, end):
         else:
             start = mid + 1
     return None
+
+n = 10
+target = 13
+array = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
 
 result = binary_search(array, target, 0, n - 1)
 if result == None:
